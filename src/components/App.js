912ddf549ca478d9179c8c2f4e5d055d86c97bec
Mohan <mohan.8991@gmail.com>
2019-09-05 00:11:32 -0400

    Route to profile and account pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 // import logo from '../logo.svg';
 // import './App.css';
 
 import Maps from "./Maps";
 import Header from "./Header";
+import Profile from "./Profile";
+import Account from "./Account";
 
 function App(){
   return (
       <Router>
           <div>
               <Header/>
-              <Route exact path="/" component={Maps} />
-              {/*<Route path="/profile" component={About} />*/}
+              <main className='container'>
+                <Route exact path="/" component={Maps} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/account" component={Account} />
+              </main>
           </div>
       </Router>
   );
